3b7322748d04e387ff2a3ed59fd44e554d708f7f
Vinayak9307 <kushwahavinayak286@gmail.com>
2024-04-20 13:29:01 +0530

    Added PicoVoice wakeup and intent capture

---
+++ android_component/a_silent_voice/lib/homepage.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:picovoice_flutter/picovoice_error.dart';
+import 'package:picovoice_flutter/picovoice_manager.dart';
+import 'package:rhino_flutter/rhino.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final String accessKey =
+      "vTOSlvjB0JX/YFEvd7s4idEVeXmOxWKdZGUowafSF2ru4k6zUxQd5A==";
+  final String keywordAsset = "./assets/zoro.ppn";
+  final String contextAsset = "./assets/mangaReader.rhn";
+
+  bool _isListening = false;
+  PicovoiceManager? _picovoiceManager;
+  
+  @override
+  void initState() {
+    super.initState();
+    _initPicovoice();
+  }
+
+  void _initPicovoice() async {
+    try {
+      _picovoiceManager = await PicovoiceManager.create(accessKey, keywordAsset,
+          _wakeWordCallback, contextAsset, _inferenceCallback,
+          processErrorCallback: _errorCallback);
+      await _picovoiceManager?.start();
+    } on PicovoiceActivationException {
+      _errorCallback(
+          PicovoiceActivationException("AccessKey activation error."));
+    } on PicovoiceActivationLimitException {
+      _errorCallback(PicovoiceActivationLimitException(
+          "AccessKey reached its device limit."));
+    } on PicovoiceActivationRefusedException {
+      _errorCallback(PicovoiceActivationRefusedException("AccessKey refused."));
+    } on PicovoiceActivationThrottledException {
+      _errorCallback(PicovoiceActivationThrottledException(
+          "AccessKey has been throttled."));
+    } on PicovoiceException catch (ex) {
+      _errorCallback(ex);
+    }
+  }
+
+  _wakeWordCallback() {
+    setState(() {
+      _isListening = true;
+    });
+    // _speak("Hello,I am zoro.");
+  }
+
+  _inferenceCallback(RhinoInference inference) {
+    if(inference.isUnderstood!){
+      Map<String , String>? slots = inference.slots!;
+      if(inference.intent == "open_manga"){
+        String? manga = slots["manga"];
+      }
+    }else{
+    }
+    setState(() {
+      _isListening = false;
+    });
+  }
+
+  _errorCallback(PicovoiceException error) {
+    print(error.message);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                child: _isListening
+                    ? Icon(
+                        Icons.mic,
+                        size: 100,
+                        color: Theme.of(context).focusColor,
+                      )
+                    : Icon(
+                        Icons.mic_none,
+                        size: 100,
+                        color: Theme.of(context).primaryColor,
+                      )),
+          ],
+        ),
+      ),
+    );
+  }
+}
